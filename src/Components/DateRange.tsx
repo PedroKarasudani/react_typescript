971536c642e52a79b8baf84fd09f0366f5e796d5
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-09-18 11:35:07 -0300

    DateInput

---
+++ src/Components/DateRange.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import DateInput from './DateInput';
+
+const DateRange = () => {
+  const [inicio, setInicio] = React.useState('');
+  const [final, setFinal] = React.useState('');
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <DateInput
+        label={'Início'}
+        onChange={({ target }) => setInicio(target.value)}
+      />
+      {inicio}
+      <DateInput
+        label={'Final'}
+        onChange={({ target }) => setFinal(target.value)}
+      />
+      {final}
+    </form>
+  );
+};
+
+export default DateRange;
