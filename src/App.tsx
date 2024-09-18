30f489b1edc73b2566aa4269366c3ed66b902ebe
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-09-17 21:32:54 -0300

    criando pastas para ambientes (PAGES , COMPONENTS e HOOKS)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Header from './Components/Header';
+import Slidenav from './Components/Slidenav';
+import Resumo from './Pages/Resumo';
 import './Style.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Slidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Slidenav.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Slidenav = () => {
+  return <div>Slidenav</div>;
+};
+
+export default Slidenav;
