87ba8da19331492d9a0305e20d81da301e9f1774
Pedro K. L. <pedro.karasudani@hotmail.com>
2024-09-23 11:42:46 -0300

    Router pagina Venda

--- src/Components/VendaItem.tsx
+++ src/Components/VendaItem.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { IVenda } from '../Context/DataContext';
+import { NavLink } from 'react-router-dom';
 
 const VendaItem = ({ venda }: { venda: IVenda }) => {
   return (
     <div className="box venda">
-      <a href="" style={{ fontFamily: 'monospace' }}>
+      <NavLink to={`/vendas/${venda.id}`} style={{ fontFamily: 'monospace' }}>
         {venda.id}
-      </a>
+      </NavLink>
       <div>{venda.nome}</div>
       <div>
         {venda.preco.toLocaleString('pt-BR', {
--- src/Pages/Venda.tsx
+++ src/Pages/Venda.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { IVenda } from '../Context/DataContext';
+import useFetch from '../Hooks/useFetch';
+import { useParams } from 'react-router-dom';
+
+type VendaSemData = Omit<IVenda, 'data'>;
 
 const Venda = () => {
-  return <div>Venda</div>;
+  const { id } = useParams();
+
+  const { data, loading } = useFetch<VendaSemData>(
+    `https://data.origamid.dev/vendas/${id}`,
+  );
+
+  if (data === null) return null;
+  return (
+    <div>
+      <div className="box mb">ID: {data.id}</div>
+      <div className="box mb">Nome: {data.nome}</div>
+      <div className="box mb">
+        Preço:{' '}
+        {data.preco.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        })}
+      </div>
+      <div className="box mb">Pagamento: {data.pagamento}</div>
+      {data.parcelas ? (
+        <div className="box mb">Parcelas: {data.parcelas} Parcelas</div>
+      ) : (
+        <div className="box mb">Parcelas: avista</div>
+      )}
+      <div className="box mb">Status: {data.status}</div>
+    </div>
+  );
 };
 
 export default Venda;
